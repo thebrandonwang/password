147c770301facd9ea62d2c03415306f23e2d12c4
Brandon Wang <thebrandonwang@gmail.com>
2022-03-29 19:00:11 +0800

    v02

--- password.py
+++ password.py
@@ -1,13 +1,14 @@
 password = 'a123456'
-chance = 2
+chance = 3
 while True:
 	answer = input('please enter your password: ')
 	if answer == password:
 		print('log in successful')
 		break
-	elif chance > 0:
+	else:
 		chance = chance-1
-		print('please try again')
-	elif chance == 0:
-		print('account locked')
-		break
+		if chance > 0:
+			print('please try again')
+		else:
+			print('account locked')
+			break
