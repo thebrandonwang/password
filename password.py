9d579b2e19b38dadcf4aef14dfc45ab0c0719f95
thebrandonwang <96467285+thebrandonwang@users.noreply.github.com>
2022-04-11 18:56:56 +0800

    v05

--- password.py
+++ password.py
@@ -1,12 +1,12 @@
 password = 'a123456'
 chance = 4
 while chance > 0:
+	chance = chance - 1
 	answer = input('please enter your password: ')
 	if answer == password:
 		print('log in successful')
 		break
 	else:
-		chance = chance-1
 		if chance > 1:
 			print('wrong passord, you still have', chance, 'chances')
 		elif chance == 1:
